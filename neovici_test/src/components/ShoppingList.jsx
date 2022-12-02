3d7cd8bd91b7493a951e920a72a820ba51437b8d
CarlozzBarry <seergejrachmaninov@gmail.com>
2022-12-02 11:26:03 +0100

    Update ShoppingList.jsx

--- neovici_test/src/components/ShoppingList.jsx
+++ neovici_test/src/components/ShoppingList.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const ShoppingList = ({  ingredients }) => {
+const ShoppingList = ({ ingredients }) => {
+  const print = (event) => {
+    alert("Shopping list: " + ingredients);
+  };
+
   return (
     <div>
       <h1>Shopping List</h1>
       {ingredients.map((ingredient, index) => (
-        <li key={ingredient + index}>
-          {ingredient}
-        </li>
+        <li key={ingredient + index}>{ingredient}</li>
       ))}
+      <button onClick={print}>Print</button>
     </div>
-  )
-}
+  );
+};
 
 export default ShoppingList;
