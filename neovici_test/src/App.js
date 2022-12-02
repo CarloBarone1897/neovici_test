095cc15b0b0314baf6c4f3a2c2d2011021b4b48e
CarlozzBarry <seergejrachmaninov@gmail.com>
2022-12-02 11:25:44 +0100

    Added CSS files for the components

--- neovici_test/src/App.js
+++ neovici_test/src/App.js
@@ -9,18 +9,17 @@ const App = () => {
 
   const changeHandler = async (event) => {
     // Concat strings with `` for better readability
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${event.target.value}`
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${event.target.value}`;
 
     try {
       // Use Asnyc/await to retrieve async data from an API
       const response = await fetch(url);
       // Await the response to get the actual data from the API
       const actualData = await response.json();
-  
-      setDrinks(actualData.drinks);
 
+      setDrinks(actualData.drinks);
     } catch (error) {
-      console.log('error while searching for a drink', error)
+      console.log("error while searching for a drink", error);
     }
   };
 
@@ -30,45 +29,38 @@ const App = () => {
     // handle duplicates in array of strings
     for (let index = 0; index < newIngredients.length; index++) {
       const ingredient = newIngredients[index];
-      
+
       if (!ingredients.includes(ingredient)) {
         ingredientToAdd.push(ingredient);
-      };
+      }
     }
 
-    setIngredients([
-      ...ingredients,
-      ...ingredientToAdd,
-    ])
-  }
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    changeHandler(event);
+    setIngredients([...ingredients, ...ingredientToAdd]);
   };
 
   return (
     // livello 1 a colonne
     <div className="main-container">
-      {/* livello 2 larghezza 100% */}
-      <form onSubmit={submitHandler}>
-        <div className="search-container">
-          <input type="search" className="search-field" onChange={changeHandler}></input>
-          <button className="submit-btn">
-            SEARCH COCKTAILS
-          </button>
-        </div>
-      </form>
+      <div id="level-2" className="searching-bar">
+        <form onChange={changeHandler}>
+          <div className="search-container">
+            <input type="search" className="search-field"></input>
+          </div>
+        </form>
+      </div>
       {/* livello 2 a righe */}
-      <div>
 
-        {/* livello 3 larghezza 70% */}
-        <DrinkList addToCart={addToCart} drinks={drinks}/>
-        {/* livello 3 larghezza 30% */}
-        <ShoppingList ingredients={ingredients} />
+      <div id="level-2" className="drinks-and-lwist">
+      <div id="level-3" className="shopping-list">
+          {/* livello 3 larghezza 30% */}
+          <ShoppingList ingredients={ingredients} />
+        </div>
+        <div id="level-3" className="drinks">
+          <DrinkList addToCart={addToCart} drinks={drinks} />
+        </div>
       </div>
     </div>
   );
-  }
+};
 
 export default App;
--- neovici_test/src/components/DrinkList.jsx
+++ neovici_test/src/components/DrinkList.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import CocktailCard from "./CocktailCard";
 
-
 // Props for DrinkList contains `drinks`
-const DrinkList = ({ drinks, addToCart }) => (
+const DrinkList = ({ drinks, addToCart }) =>
   drinks.map((drink) => (
     <CocktailCard key={drink.idDrink} drink={drink} addToCart={addToCart} />
-  ))
-);
+  ));
 
 export default DrinkList;
